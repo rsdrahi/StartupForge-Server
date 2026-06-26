e1504cfad4af88c70252c4cd2029ec2473040c8c
rsdrahi <rsdrahi13@gmail.com>
2026-06-26 06:12:56 +0600

    work on update and delete funtionality on the opportunitiescollection.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 app.use(cors())
 app.use(express.json())
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -32,12 +32,71 @@ async function run() {
 
     const database = client.db("startupforge");
     const startupCollection = database.collection("startupCollection");
+    const opportunitiesCollection = database.collection("opportunities")
+
+
+    // startup
+    app.get('/api/myStartup/:email', async (req, res) => {
+      const { email } = req.params;
+      const result = await startupCollection.findOne({ email })
+      res.send(result);
+    })
 
     app.post('/api/myStartup', async (req, res) => {
       const startUp = req.body
       const result = await startupCollection.insertOne(startUp);
       res.send(result);
     })
+
+    // opportunity
+
+    app.post('/api/addOpportunity', async (req, res) => {
+      const opportunity = req.body;
+      const result = await opportunitiesCollection.insertOne(opportunity)
+      res.send(result);
+    })
+
+      app.get('/api/addOpportunity/:founderId', async (req, res) => {
+        const { founderId } = req.params;
+        // console.log(founderId, "Received Id");
+        const result = await opportunitiesCollection.find({ founderId }).toArray();
+        console.log(result, "result");
+        res.send(result);
+    })
+
+    app.patch('/api/addOpportunity/:id', async (req, res) => {
+      const { id } = req.params;
+      const updateData = req.body;
+      const result = await opportunitiesCollection.updateOne(
+        { _id: new ObjectId(id) },
+        {
+          $set: {
+            ...updateData,
+          },
+        }
+      )
+      res.send(result);
+    })
+
+    app.delete('/api/addOpportunity/:id', async (req, res) => {
+      const { id } = req.params;
+      const result = await opportunitiesCollection.deleteOne({ _id: new ObjectId(id) })
+      res.send(result)
+    })
+
+    // app.patch('/api/myStartup/:id', async (req, res) => {
+    //   const startUp = req.body
+    //   const result = await startupCollection.updateOne({
+    //     _id: new ObjectId(id)
+    //   },
+    //     {
+    //       $set: {
+    //         startUp,
+    //       }
+    //     }
+    //   );
+    //   res.send(result);
+    // })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
